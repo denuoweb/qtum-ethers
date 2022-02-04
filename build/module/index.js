24dbcdc2e78459850b0180048c924d7821a73d8a
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-04 04:24:03 -0800

    port to htmlcoin

--- build/module/index.js
+++ build/module/index.js
@@ -1,6 +1,6 @@
-export * from "./lib/QtumProvider";
-export * from "./lib/QtumFunctionProvider";
-export * from "./lib/QtumContractFactory";
-export * from "./lib/QtumWallet";
+export * from "./lib/HtmlcoinProvider";
+export * from "./lib/HtmlcoinFunctionProvider";
+export * from "./lib/HtmlcoinContractFactory";
+export * from "./lib/HtmlcoinWallet";
 export * from "./lib/helpers/utils";
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsY0FBYyxvQkFBb0IsQ0FBQztBQUNuQyxjQUFjLDRCQUE0QixDQUFDO0FBQzNDLGNBQWMsMkJBQTJCLENBQUM7QUFDMUMsY0FBYyxrQkFBa0IsQ0FBQztBQUNqQyxjQUFjLHFCQUFxQixDQUFDIn0=
