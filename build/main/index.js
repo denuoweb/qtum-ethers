7d17099ab1cf573d36cba169d1920b85fabc5f7c
Jaron Rosenau <jaron.rosenau@gmail.com>
2022-02-04 04:51:10 -0800

    port to htmlcoin

--- build/main/index.js
+++ build/main/index.js
@@ -10,9 +10,9 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./lib/QtumProvider"), exports);
-__exportStar(require("./lib/QtumFunctionProvider"), exports);
-__exportStar(require("./lib/QtumContractFactory"), exports);
-__exportStar(require("./lib/QtumWallet"), exports);
+__exportStar(require("./lib/HtmlcoinProvider"), exports);
+__exportStar(require("./lib/HtmlcoinFunctionProvider"), exports);
+__exportStar(require("./lib/HtmlcoinContractFactory"), exports);
+__exportStar(require("./lib/HtmlcoinWallet"), exports);
 __exportStar(require("./lib/helpers/utils"), exports);
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQUEscURBQW1DO0FBQ25DLDZEQUEyQztBQUMzQyw0REFBMEM7QUFDMUMsbURBQWlDO0FBQ2pDLHNEQUFvQyJ9
